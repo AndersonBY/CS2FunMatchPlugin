f75d1b4037d2ccbf61becfd2011337f88d580d69
Anderson <andersonby@163.com>
2024-10-08 20:18:14 +0800

    Improve soccer ball handling and update plugin version

--- FunFootBall.cs
+++ FunFootBall.cs
@@ -12,13 +12,13 @@ public class FunFootBall : FunBaseClass
 {
     public override string Decription => "FootBall Mode T aims to take soccerball to CTspawn 足球模式 T需要将球踢进CT出生点";
 
-    private CPhysicsProp ?SoccerBall;
-    private Timer ?BallStatusTimer;
-    private Vector ScoreCenter = new(0,0,0);
-    private Vector SoccerLastPosition = new(0,0,0);
-    private Vector SoccerSpawnPosition = new(0,0,0);
+    private CPhysicsProp? SoccerBall;
+    private Timer? BallStatusTimer;
+    private Vector ScoreCenter = new(0, 0, 0);
+    private Vector SoccerLastPosition = new(0, 0, 0);
+    private Vector SoccerSpawnPosition = new(0, 0, 0);
     private int soccer_same_pos_times = 0;
-    private BasePlugin.GameEventHandler<EventRoundFreezeEnd> ?EventRoundFreezeEndHandler;
+    private BasePlugin.GameEventHandler<EventRoundFreezeEnd>? EventRoundFreezeEndHandler;
     public override void EndFun(FunMatchPlugin plugin)
     {
         Enabled = false;
@@ -26,17 +26,17 @@ public class FunFootBall : FunBaseClass
         ConVar.Find("mp_autokick")!.SetValue(true);
         ConVar.Find("mp_buy_during_immunity")!.SetValue(0);
         if (EventRoundFreezeEndHandler is not null)
-        plugin.DeregisterEventHandler(EventRoundFreezeEndHandler!);
+            plugin.DeregisterEventHandler(EventRoundFreezeEndHandler!);
         if (BallStatusTimer is not null)
-        BallStatusTimer!.Kill();
+            BallStatusTimer!.Kill();
     }
     public override void Fun(FunMatchPlugin plugin)
     {
         if (Enabled) return;
         Enabled = true;
-        ScoreCenter = new(0,0,0);
-        SoccerLastPosition = new(0,0,0);
-        SoccerSpawnPosition = new(0,0,0);
+        ScoreCenter = new(0, 0, 0);
+        SoccerLastPosition = new(0, 0, 0);
+        SoccerSpawnPosition = new(0, 0, 0);
         soccer_same_pos_times = 0;
         ConVar.Find("mp_buytime")!.SetValue(0.0f);
         ConVar.Find("mp_autokick")!.SetValue(false);
@@ -63,7 +63,7 @@ public class FunFootBall : FunBaseClass
         ScoreCenter.Z = ScoreCenter.Z/gameRules.SpawnPointCount_CT;
         Console.WriteLine($"scorecenter{ScoreCenter.X} {ScoreCenter.Y} {ScoreCenter.Z}");
         */
-        plugin.RegisterEventHandler<EventRoundFreezeEnd>(EventRoundFreezeEndHandler = (@event, info)=>
+        plugin.RegisterEventHandler<EventRoundFreezeEnd>(EventRoundFreezeEndHandler = (@event, info) =>
         {
             var Allplayers = Utilities.GetPlayers();
             bool HasGivenBall = false;
@@ -78,7 +78,7 @@ public class FunFootBall : FunBaseClass
                     HasGivenBall = true;
                     SoccerBall = Utilities.CreateEntityByName<CPhysicsProp>("prop_physics_multiplayer")!;
                     var pawn = p.PlayerPawn.Get();
-                    Vector position = new(pawn!.AbsOrigin!.X,pawn!.AbsOrigin!.Y,pawn!.AbsOrigin!.Z + 100);
+                    Vector position = new(pawn!.AbsOrigin!.X, pawn!.AbsOrigin!.Y, pawn!.AbsOrigin!.Z + 100);
                     SoccerBall.Teleport(position);
                     SoccerBall.SetModel("models/props/de_dust/hr_dust/dust_soccerball/dust_soccer_ball001.vmdl");
                     //models/props/de_dust/hr_dust/dust_soccerball/dust_soccer_ball001.vmdl_c
@@ -86,7 +86,7 @@ public class FunFootBall : FunBaseClass
                     SoccerSpawnPosition.Y = SoccerBall!.AbsOrigin!.Y;
                     SoccerSpawnPosition.Z = SoccerBall!.AbsOrigin!.Z;
                     //SoccerBall.GlowColor = Color.GreenYellow;
-                    
+
                     SoccerBall.DispatchSpawn();
                 }
                 if (p.PawnIsAlive && p.Team == CsTeam.CounterTerrorist && HasSetScorePoint == false)
@@ -104,11 +104,11 @@ public class FunFootBall : FunBaseClass
             }
             //CCSPlayerResource siteA siteB position?
             //Utilities.FindAllEntitiesByDesignerName<CCSPlayerResource>("???");
-            BallStatusTimer = plugin.AddTimer(1,()=>CheckBallStatus(),TimerFlags.REPEAT);
+            BallStatusTimer = plugin.AddTimer(1, () => CheckBallStatus(), TimerFlags.REPEAT);
             return HookResult.Stop;
         });
 
-        plugin.RegisterEventHandler<EventRoundEnd>((@event, info)=>
+        plugin.RegisterEventHandler<EventRoundEnd>((@event, info) =>
         {
             EndFun(plugin);
             return HookResult.Stop;
@@ -117,6 +117,16 @@ public class FunFootBall : FunBaseClass
 
     private void CheckBallStatus()
     {
+        if (Enabled == false)
+        {
+            BallStatusTimer!.Kill();
+            return;
+        }
+        if (SoccerBall == null)
+        {
+            BallStatusTimer!.Kill();
+            return;
+        }
         if (IsBallIn())
         {
             TWin();
@@ -125,7 +135,7 @@ public class FunFootBall : FunBaseClass
         if (IsBallFalling() || IsBallStuck())
         {
             if (SoccerBall!.IsValid)
-            SoccerBall.Teleport(SoccerSpawnPosition);
+                SoccerBall.Teleport(SoccerSpawnPosition);
             SoccerLastPosition.X = SoccerSpawnPosition.X;
             SoccerLastPosition.Y = SoccerSpawnPosition.Y;
             SoccerLastPosition.Z = SoccerSpawnPosition.Z;
@@ -135,11 +145,11 @@ public class FunFootBall : FunBaseClass
     private bool IsBallIn()
     {
         if (!SoccerBall!.IsValid) return false;
-        var ball_x  = SoccerBall.AbsOrigin!.X;
-        var ball_y  = SoccerBall.AbsOrigin!.Y;
-        var ball_z  = SoccerBall.AbsOrigin!.Z;
-        if (Math.Abs(ScoreCenter.X-ball_x) <= 500 && Math.Abs(ScoreCenter.Y-ball_y) <= 500 && Math.Abs(ScoreCenter.Z-ball_z) <= 50)
-        return true;
+        var ball_x = SoccerBall.AbsOrigin!.X;
+        var ball_y = SoccerBall.AbsOrigin!.Y;
+        var ball_z = SoccerBall.AbsOrigin!.Z;
+        if (Math.Abs(ScoreCenter.X - ball_x) <= 500 && Math.Abs(ScoreCenter.Y - ball_y) <= 500 && Math.Abs(ScoreCenter.Z - ball_z) <= 50)
+            return true;
         else return false;
     }
 
@@ -148,14 +158,14 @@ public class FunFootBall : FunBaseClass
         var gameRulesProxie = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules");
         var gameRules = gameRulesProxie.First().GameRules!;
         if (gameRules!.WarmupPeriod)
-        return;
+            return;
         var Allplayers = Utilities.GetPlayers();
         foreach (var p in Allplayers)
         {
             if (!p.IsValid) continue;
             if (p.Team == CsTeam.CounterTerrorist)
             {
-                p.CommitSuicide(false,true);
+                p.CommitSuicide(false, true);
             }
         }
     }
@@ -178,7 +188,7 @@ public class FunFootBall : FunBaseClass
             Server.PrintToChatAll(StringExtensions.ReplaceColorTags("{RED}") + "[FootBallMode] " + "Ball Stuck Resetting");
             return true;
         }
-            
+
         return false;
     }
 
